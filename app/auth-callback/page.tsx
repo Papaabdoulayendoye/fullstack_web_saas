20b6d9d79e772fcac5b7fe31b48a48cdb58107fc
Papaabdoulaye <papaabdoulayepipo@gmail.com>
2023-11-28 16:53:13 +0000

    begin with prisma

--- app/auth-callback/page.tsx
+++ app/auth-callback/page.tsx
@@ -1,9 +1,27 @@
+"use client"
+import React from "react";
+import { Loader, Loader2 } from "lucide-react";
+import { useSearchParams, redirect, useRouter } from 'next/navigation';
+import { trpc } from '../_trpc/client';
 const AuthCallback = () => {
-    return (
-        <div className='w-full max-h-screen'>
-            
+    const searchParams = useSearchParams()
+    const origin = searchParams.get('origin')
+    const router = useRouter()
+    
+    const {data, isLoading} = trpc.authCallback.useQuery(undefined,{
+        onSuccess : ({success}) => {
+            if (success) {
+                router.push(origin ? `/${origin}`: '/dashboard')
+            }
+        }
+    })
+    
+    return( 
+        <div className='flex flex-col items-center justify-center mt-10'>
+            <h1 className=' text-4xl text-black'>You 'll be redirect soon...</h1>
+            <Loader2 className=' h-[40px] w-[40px]' />
         </div>
     )
-}
+};
 
-export default AuthCallback
+export default AuthCallback;
