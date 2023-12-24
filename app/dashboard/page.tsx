97ad31294b6d9ac810209d9f4634c66d3f1aea91
Papaabdoulaye <papaabdoulayepipo@gmail.com>
2023-12-24 21:37:21 +0000

    fix unable add use to the db

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,27 +1,16 @@
 import MaxWidthWrapper from '@/components/MaxWidthWrapper'
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
-import { db } from '@/lib/utils';
 import Dashboard from '@/components/Dashboard';
 
 const Page = async () => {
-    const {getUser, isAuthenticated} = getKindeServerSession()
+    const {isAuthenticated} = getKindeServerSession()
     const userIsConnected = isAuthenticated()
-    const user = getUser()
     if (!userIsConnected) {
         return redirect('/auth-callback?origin=dashboard')
     }
-    
-    // const currentUser = await db.user.findFirstOrThrow({
-    //     where : 
-    //     {
-    //         userId : user?.id!
-    //     }
-    // })
-    
     return (
         <MaxWidthWrapper>
-            <h1>{user.email}</h1>
             <Dashboard />
         </MaxWidthWrapper>
     )
